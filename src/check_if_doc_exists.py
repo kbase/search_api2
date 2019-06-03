7d721eb08d594ab7152c4162ddf0be8294afe0f0
slebras <slebras@lbl.gov>
2019-06-03 11:27:31 -0700

    adding changes requested: jsonschema update, sanic update

--- src/check_if_doc_exists.py
+++ src/check_if_doc_exists.py
@@ -1,4 +1,16 @@
-import requests
+import request
+import jsonschema
+from sanic.exceptions import NotFound
+
+schema = {
+    'type': 'object'
+    'properties': {
+        'doc_id': {'type': 'string'},
+        'index': {'type': 'string'},
+        'es_datatype': {'type': 'string'}
+    } 
+}
+
 
 def check_if_doc_exists(params, headers, config):
     """
@@ -9,34 +21,20 @@ def check_if_doc_exists(params, headers, config):
         es_datatype - elasticsearch document data type
     """
     # verify inputs
-    if not params.get("doc_id"):
-        # bad
-        pass
-    if not params.get('index'):
-        # bad
-        pass
-    if not params.get('es_datatype'):
-        # bad
-        pass
-
-    doc_id = params['doc_id']
+    jsonschema.validate(instance=params, schema=schema)
     index = config['index_prefix'] + '.' + params['index']
-    es_datatype = params.get('es_datatype', 'data')
 
     resp = requests.head(
-        config['elasticsearch_url'] + '/' + index + '/' + es_datatype + '/' + doc_id
+        config['elasticsearch_url'] + '/' + index + '/' + params.get('es_datatype', 'data') + '/' + params['doc_id']
     )
     if resp.status_code == 200:
         return {
             'result': True
         }
-    elif resp.status_code == 404:
-        # does not exist.
-        return {
-            'result': False
-        }
     else:
-        # error
-        return {
-            'error': f'returned with status code: {resp.status_code}'
-        }
+        # error we are not interested in...
+        raise RuntimeError(resp.text)
+
+@app.exception(NotFound)
+async def I_AM_NOT_FOUND_WOWWIIEEE(request, exception):
+    return exception
