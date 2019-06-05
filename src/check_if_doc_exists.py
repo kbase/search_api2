803f9fe04c5a03753a5939cca5407ffe86e0185e
Jay R Bolton <jayrbolton@gmail.com>
2019-06-05 14:35:16 -0700

    Clean up

--- src/check_if_doc_exists.py
+++ src/check_if_doc_exists.py
@@ -2,7 +2,7 @@ import requests
 import jsonschema
 import sanic
 
-schema = {
+_SCHEMA = {
     'type': 'object',
     'properties': {
         'doc_id': {'type': 'string'},
@@ -20,16 +20,14 @@ def check_if_doc_exists(params, headers, config):
         index - elasticsearch index to search (required)
         es_datatype - elasticsearch document data type
     """
-    # verify inputs
-    jsonschema.validate(instance=params, schema=schema)
+    jsonschema.validate(instance=params, schema=_SCHEMA)
     index = config['index_prefix'] + '.' + params['index']
-
     resp = requests.head(
         config['elasticsearch_url'] + '/' + index + '/' + params.get('es_datatype', 'data') + '/' + params['doc_id']
     )
     if resp.status_code == 200:
         return resp
-    if resp.status_code == 404:
+    elif resp.status_code == 404:
         raise sanic.exceptions.NotFound(f"Document with ID '{params['doc_id']}' does not exist.")
     else:
         # error we are not interested in...
