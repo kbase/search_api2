40ddcbfea55e5948ead2b5bfa933d454a7ba424e
Erik Pearson <eapearson@lbl.gov>
2020-03-13 08:50:40 -0700

    add user profile endpoint url to config

--- src/utils/config.py
+++ src/utils/config.py
@@ -10,6 +10,7 @@ def init_config():
     Initialize configuration data for the whole app
     """
     ws_url = os.environ.get('WORKSPACE_URL', 'https://ci.kbase.us/services/ws').strip('/')
+    user_profile_url = os.environ.get('USER_PROFILE_URL', 'https://ci.kbase.us/services/user_profile/rpc').strip('/')
     es_url = os.environ.get('ELASTICSEARCH_URL', 'http://elasticsearch:9200').strip('/')
     index_prefix = os.environ.get('INDEX_PREFIX', 'test')
     config_url = os.environ.get('GLOBAL_CONFIG_URL', 'https://github.com/kbase/index_runner_spec/releases/latest/download/config.yaml')  # noqa
@@ -23,5 +24,6 @@ def init_config():
         'global': global_config,
         'elasticsearch_url': es_url,
         'index_prefix': index_prefix,
-        'workspace_url': ws_url
+        'workspace_url': ws_url,
+        'user_profile_url': user_profile_url
     }
