c80fb15d62f9a7af829df8ecc31f16b46b50326e
Jay R Bolton <jayrbolton@gmail.com>
2020-07-16 13:06:31 -0700

    Allow running of integration tests against remote servers

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -18,29 +18,32 @@ from tests.integration.data import (
 )
 
 
-APP_URL = 'http://localhost:5000'
-ES_URL = os.environ.get("ES_URL", "http://localhost:9500")
-# TOKEN = os.environ["TOKEN"]
-INDEX_PREFIX = os.environ.get('INDEX_PREFIX', 'search2')
-USER_PROFILE_URL = os.environ.get('USER_PROFILE_URL', 'https://ci.kbase.us/services/user_profile')
-WS_URL = os.environ.get('WS_URL', 'https://ci.kbase.us/services/ws')
-IMAGE_NAME = "kbase/search2"
+APP_URL = os.environ.get("APP_URL", 'http://localhost:5001')
 
-# Start the services
+if not os.environ.get("SKIP_DOCKER"):
+    ES_URL = os.environ.get("ES_URL", "http://localhost:9500")
+    # TOKEN = os.environ["TOKEN"]
+    INDEX_PREFIX = os.environ.get('INDEX_PREFIX', 'search2')
+    USER_PROFILE_URL = os.environ.get('USER_PROFILE_URL', 'https://ci.kbase.us/services/user_profile')
+    WS_URL = os.environ.get('WS_URL', 'https://ci.kbase.us/services/ws')
+    IMAGE_NAME = "kbase/search2"
+    # Build and start the app using docker
+    cmd = f"""
+    docker build . -t {IMAGE_NAME} && \\
+    docker run -e ELASTICSEARCH_URL={ES_URL} \\
+               -e WORKSPACE_URL={WS_URL} \\
+               -e USER_PROFILE_URL={USER_PROFILE_URL} \\
+               -e WORKERS=2 \\
+               -e INDEX_PREFIX={INDEX_PREFIX} \\
+               -e INDEX_PREFIX_DELIMITER=. \\
+               -e DEVELOPMENT=1 \\
+               --network host \\
+               {IMAGE_NAME}
+    """
+    print(f'Running command:\n{cmd}')
+    subprocess.Popen(cmd, shell=True)
+
 # This implicitly tests the "/" path
-cmd = f"""
-docker build . -t {IMAGE_NAME} && \\
-docker run -e ELASTICSEARCH_URL={ES_URL} \\
-           -e WORKSPACE_URL={WS_URL} \\
-           -e USER_PROFILE_URL={USER_PROFILE_URL} \\
-           -e WORKERS=2 \\
-           -e INDEX_PREFIX={INDEX_PREFIX} \\
-           -e INDEX_PREFIX_DELIMITER=. \\
-           --network host \\
-           {IMAGE_NAME}
-"""
-print(f'Running command:\n{cmd}')
-subprocess.Popen(cmd, shell=True)
 wait_for_service(APP_URL, "search2")
 
 
@@ -88,6 +91,7 @@ def test_search_example3():
         data=json.dumps(search_request3),
     )
     data = resp.json()
+    print('data', data)
     assert data['jsonrpc'] == search_response3['jsonrpc']
     assert data['id'] == search_response3['id']
     assert len(data['result']) == 1
