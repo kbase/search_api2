67c7e1ef603b03f492ab3ef5bfd287424adbfd9e
Erik Pearson <eapearson@lbl.gov>
2021-03-02 08:53:32 -0800

    re-name variable already used in higher scope (lint warning)

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -8,11 +8,11 @@ def init_logger():
     Initialize log settings. Mutates the `logger` object.
     """
     logging.getLogger("urllib3").setLevel(logging.WARNING)
-    logger = logging.getLogger('search2')
+    search2_logger = logging.getLogger('search2')
     # Set the log level
     level = os.environ.get('LOGLEVEL', 'DEBUG').upper()
-    logger.setLevel(level)
-    logger.propagate = False  # Don't print duplicate messages
+    search2_logger.setLevel(level)
+    search2_logger.propagate = False  # Don't print duplicate messages
     logging.basicConfig(level=level)
     # Create the formatter
     fmt = "%(asctime)s %(levelname)-8s %(message)s (%(filename)s:%(lineno)s)"
@@ -21,8 +21,8 @@ def init_logger():
     # Stdout
     stdout_handler = logging.StreamHandler(sys.stdout)
     stdout_handler.setFormatter(formatter)
-    logger.addHandler(stdout_handler)
-    print(f'Logger and level: {logger}')
+    search2_logger.addHandler(stdout_handler)
+    print(f'Logger and level: {search2_logger}')
     print(f'''
 ** To see more or less logging information, adjust the
 ** log level by setting the LOGLEVEL environment variable
@@ -31,7 +31,7 @@ def init_logger():
 ** It is currently set to:
 **   {level}
 ''')
-    return logger
+    return search2_logger
 
 
 logger = init_logger()
