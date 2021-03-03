affa8b1b28399fa3c9de409260cafe022268f473
Erik Pearson <eapearson@lbl.gov>
2021-03-03 06:52:48 -0800

    surface timing constants, add additional test, for wait_for_service.

--- src/utils/wait_for_service.py
+++ src/utils/wait_for_service.py
@@ -3,18 +3,22 @@ import time
 
 from src.utils.logger import logger
 
+DEFAULT_TIMEOUT = 180
+WAIT_POLL_INTERVAL = 5
 
-def wait_for_service(url, name, timeout=180):
+
+def wait_for_service(url, name, timeout=DEFAULT_TIMEOUT):
     start = time.time()
     while True:
         logger.info(f'Attempting to connect to {name} at {url}')
         try:
-            requests.get(url).raise_for_status()
+            requests.get(url, timeout=timeout).raise_for_status()
             logger.info(f'{name} is online!')
             break
         except Exception:
             logger.info(f'Waiting for {name} at {url}')
-            if time.time() - start > timeout:
-                logger.error(f'Unable to connect to {name} at {url}')
+            total_elapsed = time.time() - start
+            if total_elapsed > timeout:
+                logger.error(f'Unable to connect to {name} at {url} after {total_elapsed} seconds')
                 exit(1)
-            time.sleep(5)
+            time.sleep(WAIT_POLL_INTERVAL)
--- tests/unit/utils/test_wait_for_service.py
+++ tests/unit/utils/test_wait_for_service.py
@@ -1,7 +1,9 @@
-from src.utils.wait_for_service import wait_for_service
+from src.utils.wait_for_service import wait_for_service, WAIT_POLL_INTERVAL
 import pytest
 import logging
 import time
+
+MINIMAL_CALL_TIME = 1
 
 
 def test_init_config_invalid_config_url(caplog):
@@ -9,14 +11,35 @@ def test_init_config_invalid_config_url(caplog):
     # This ensures that logs are propagated and can be captured by caplog
     search2_logger.propagate = True
     with caplog.at_level(logging.INFO, logger='search2'):
+        start = time.time()
         with pytest.raises(SystemExit) as se:
             wait_for_service('https://foo.bar.baz', 'foo', timeout=0)
-        start = time.time()
         assert se.type == SystemExit
         assert se.value.code == 1
-        # If it took less than 10 seconds, we triggered the exist on timeout
-        # on the first check.
-        assert time.time() - start < 5
+        # If it took less than 5 seconds, we triggered the exit
+        # on the first check in the while loop.
+        elapsed = time.time() - start
+        assert elapsed < MINIMAL_CALL_TIME
+        assert 'Attempting to connect to foo at https://foo.bar.baz' in caplog.text
+        assert 'Unable to connect to foo at https://foo.bar.baz' in caplog.text
+    search2_logger.propagate = False
+
+
+def test_init_config_invalid_config_url_longer_timeout(caplog):
+    search2_logger = logging.getLogger('search2')
+    # This ensures that logs are propagated and can be captured by caplog
+    search2_logger.propagate = True
+    with caplog.at_level(logging.INFO, logger='search2'):
+        start = time.time()
+        with pytest.raises(SystemExit) as se:
+            wait_for_service('https://foo.bar.baz', 'foo', timeout=10)
+        assert se.type == SystemExit
+        assert se.value.code == 1
+        # If it took less than 5 seconds, we triggered the exit
+        # on the first check in the while loop.
+        elapsed = time.time() - start
+        assert elapsed > 10
+        assert elapsed < 10 + MINIMAL_CALL_TIME
         assert 'Attempting to connect to foo at https://foo.bar.baz' in caplog.text
         assert 'Unable to connect to foo at https://foo.bar.baz' in caplog.text
     search2_logger.propagate = False
