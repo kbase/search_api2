9c5c80a5984f197625b77461e9df60e33ddbdb6e
Erik Pearson <eapearson@lbl.gov>
2021-04-19 16:24:14 -0700

    add additional errors - No user profile, No access group (workspace id)

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,6 +1,8 @@
 from src.utils.obj_utils import get_path
 
 
+# TODO: we should use the jsonrpc11base libraries base exception for
+# server errors
 class ResponseError(Exception):
 
     def __init__(self, code=-32000, message='Server error'):
@@ -8,10 +10,21 @@ class ResponseError(Exception):
         self.jsonrpc_code = code
 
 
-class UnknownType(ResponseError):
+class AuthError(ResponseError):
 
-    def __init__(self, message) -> object:
-        super().__init__(code=-32005, message=message)
+    def __init__(self, auth_resp_json, resp_text):
+        # Extract the error message from the auth service's RPC response
+        msg = get_path(auth_resp_json, ['error', 'message'])
+        if msg is None:
+            # Fall back to the full response body
+            msg = resp_text
+        super().__init__(code=-32001, message=msg)
+
+
+class UnknownIndex(ResponseError):
+
+    def __init__(self, message):
+        super().__init__(code=-32002, message=message)
 
 
 class ElasticsearchError(ResponseError):
@@ -19,12 +32,6 @@ class ElasticsearchError(ResponseError):
     def __init__(self, message):
         msg = f"Elasticsearch request error:\n{message}"
         super().__init__(code=-32003, message=msg)
-
-
-class UnknownIndex(ResponseError):
-
-    def __init__(self, message):
-        super().__init__(code=-32002, message=message)
 
 
 class UserProfileError(ResponseError):
@@ -36,12 +43,28 @@ class UserProfileError(ResponseError):
         super().__init__(code=-32004, message=msg)
 
 
-class AuthError(ResponseError):
+class UnknownType(ResponseError):
 
-    def __init__(self, resp_json, resp_text):
-        # Extract the error message from the RPC response
-        msg = get_path(resp_json, ['error', 'message'])
-        if msg is None:
-            # Fall back to the full response body
-            msg = resp_text
-        super().__init__(code=-32001, message=msg)
+    def __init__(self, message) -> object:
+        super().__init__(code=-32005, message=message)
+
+
+class NoAccessGroupError(ResponseError):
+    """
+    Raised when a search result does not contain an "access_group"
+    key, which should be impossible.
+    """
+
+    def __init__(self):
+        message = 'A search document does not contain an access group'
+        super().__init__(code=-32006, message=message)
+
+
+class NoUserProfileError(ResponseError):
+    """
+    Raised when a username does not have an associated user profile.
+    """
+
+    def __init__(self, username):
+        message = f'A user profile could not be found for "{username}"'
+        super().__init__(code=-32007, message=message)
