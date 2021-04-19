2122ab6a24e44ba9502779b8b3d34e34ce773cd4
Erik Pearson <eapearson@lbl.gov>
2021-04-19 16:22:19 -0700

    iso8601 to epoch time now in ms; previously it was in seconds and converted to ms elsewhere

--- src/utils/formatting.py
+++ src/utils/formatting.py
@@ -1,5 +1,5 @@
 from datetime import datetime
 
 
-def iso8601_to_epoch(time_string):
-    return round(datetime.strptime(time_string, '%Y-%m-%dT%H:%M:%S%z').timestamp())
+def iso8601_to_epoch_ms(time_string):
+    return round(datetime.strptime(time_string, '%Y-%m-%dT%H:%M:%S%z').timestamp() * 1000)
