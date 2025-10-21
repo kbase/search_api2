93b7b6850b593ff938b1d99673370c906ff25ae4
bio-boris <bio-boris@github.com>
2025-10-20 20:54:35 -0500

    Tests

--- tests/helpers/unit_setup.py
+++ tests/helpers/unit_setup.py
@@ -24,6 +24,11 @@ def start_service(wait_for_url, wait_for_name):
     global container_out
     global container_err
 
+    # Set environment variables for the tests to use same auth as docker-compose
+    os.environ.setdefault('ELASTICSEARCH_URL', 'http://localhost:9200')
+    os.environ.setdefault('ELASTICSEARCH_AUTH_USERNAME', 'elastic')
+    os.environ.setdefault('ELASTICSEARCH_AUTH_PASSWORD', 'changeme')
+
     cmd = "docker compose --ansi never up"
     logger.info(f'Running command:\n{cmd}')
     container_out = open("container.out", "w")
